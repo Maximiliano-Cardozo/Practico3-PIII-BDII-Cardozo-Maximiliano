d6c9cca042b2638c48f697f2477b312fb7ae07ac
Maximiliano Cardozo <rodrigoparado@gmail.com>
2020-08-05 23:25:13 -0300

    Capas

--- Practico3/Visual/FrmMenu.vb
+++ Practico3/Visual/FrmMenu.vb
@@ -10,11 +10,18 @@ Public Class FrmMenu
 
     Private Sub BajaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MnuPersonaBaja.Click
         Dim Formulario As New FrmBajaPersona
+
         Formulario.Show()
     End Sub
 
     Private Sub ModificarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MnuPersonaModificar.Click
         Dim Formulario As New FrmModificarPersona
+        Formulario.TbxNombre.Enabled = False
+        Formulario.TbxApellido.Enabled = False
+        Formulario.TbxMail.Enabled = False
+        Formulario.TbxTelefono.Enabled = False
+        Formulario.BtnModificar.Enabled = False
+
         Formulario.Show()
     End Sub
 
@@ -35,6 +42,11 @@ Public Class FrmMenu
 
     Private Sub ModificarToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles MnuServicioModificar.Click
         Dim Formulario As New FrmModificarServicio
+        Formulario.TbxNombre.Enabled = False
+        Formulario.TbxCostoMensual.Enabled = False
+        Formulario.ListTipo.Enabled = False
+        Formulario.BtnModificar.Enabled = False
+
         Formulario.Show()
     End Sub
 
@@ -55,6 +67,9 @@ Public Class FrmMenu
 
     Private Sub ModificarToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles MnuContrataModificar.Click
         Dim Formulario As New FrmModificarContrata
+        Formulario.TbxFechaContratacion.Enabled = False
+        Formulario.TbxFechaFinContrato.Enabled = False
+        Formulario.BtnModificar.Enabled = False
         Formulario.Show()
     End Sub
 
